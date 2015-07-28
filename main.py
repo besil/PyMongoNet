e177bec457b176fc9e3455ef2e28dc24d96d1f3e
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2015-07-28 14:59:11 +0200

    Refact

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ __author__ = 'besil'
 
 if __name__ == '__main__':
     filename = "data/petster.dat"
-    
-    g = acquire_graph(filename, graph_name="petster", split=lambda l: l.split(" "), skip=lambda l: l[0] == "%")
+
+    g = acquire_graph(filename, db_name="petster", split=lambda l: l.split(" "), skip=lambda l: l[0] == "%")
 
     print(g)
--- utils.py
+++ utils.py
@@ -1,15 +1,42 @@
+import datetime
+
+from pymongo import MongoClient
+
 from core import Graph
 
 __author__ = 'besil'
 
 def lines(filename, skip=lambda line: False, split=lambda line: line):
+    totLines = 0
     with open(filename, 'r') as fin:
         for line in fin:
+            totLines += 1
+
+    # widgets = [FormatLabel('Processed: %(value)d lines (in: %(elapsed)s)')]
+    # p = ProgressBar(maxval=totLines).start()
+    count = 0
+    batch_size = 3000
+    with open(filename, 'r') as fin:
+        start = datetime.datetime.now()
+        for line in fin:
+            count += 1
+            if (count % batch_size == 0):
+                end = datetime.datetime.now()
+                print(
+                    "Consumed {} lines in {}.{}s".format(batch_size, (end - start).seconds, (end - start).microseconds),
+                    end="\r")
+                start = datetime.datetime.now()
+
+            # p.update(count)
             if not skip(line):
                 yield split(line.strip())
 
-def acquire_graph(filename, graph_name="database", split=lambda line: line, skip=lambda line: False):
-    g = Graph(db_name=graph_name)
+
+def acquire_graph(filename, db_name="database", split=lambda line: line, skip=lambda line: False):
+    cl = MongoClient()
+    cl.drop_database(db_name)
+
+    g = Graph(db_name=db_name)
 
     for line in lines(filename, skip=skip, split=split):
         g.add_edge(src=line[0], dst=line[1])
